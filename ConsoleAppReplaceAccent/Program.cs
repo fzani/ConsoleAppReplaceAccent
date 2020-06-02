64446d57170b22bf67386063ac063b76f00019a6
Felipe Zani de Jesus Lima <felipe.lima@cesgranrio.org.br>
2020-06-02 09:59:50 -0300

    Add project files.

---
+++ ConsoleAppReplaceAccent/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppReplaceAccent
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var path = @"D:\Documentos\Cesgranrio\Evento Alinhamento\2 - Normalizado";
+            var itens = Directory.GetFiles(path, "*.xlsx");
+            Parallel.ForEach(itens, a => RenameFiles(a));
+        }
+
+        static void RenameFiles(string originalPath)
+        {
+            var destinyPath = RemoveDiacritics(originalPath);
+            Console.WriteLine($"{originalPath} - {destinyPath}");
+            File.Move(originalPath, destinyPath);
+        }
+
+        static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory == UnicodeCategory.NonSpacingMark)
+                {
+                    continue;
+                }
+                stringBuilder.Append(c);
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
